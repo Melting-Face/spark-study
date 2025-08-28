37a0a314ea42b8660026053fcdf28a4bb6421dfc
jin.js <jin.js@kidsnote.com>
2025-08-29 08:07:29 +0900

    mod : executors 수 조정

--- standalone_iceberg/spark/dockerfile.d/src/setup.sh
+++ standalone_iceberg/spark/dockerfile.d/src/setup.sh
@@ -6,6 +6,8 @@ apt-get install wget -y && \
   apt-get autoremove --purge && \
   rm -rf /var/lib/apt/lists/*
 
+pip install --no-cache-dir -r $SPARK_HOME/requirements.txt
+
 while read -r line
 do
   wget "https://repo1.maven.org/maven2/$line" -P /opt/spark/jars
