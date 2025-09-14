69f7214e8b8816a5288e5af8361540050a8075d9
Melting-Face <ymist17@gmail.com>
2025-09-15 00:12:07 +0900

    add mlflow

--- standalone_hive/spark/dockerfile.d/src/entrypoint.sh
+++ standalone_hive/spark/dockerfile.d/src/entrypoint.sh
@@ -5,5 +5,5 @@
   --master spark://spark-master:7077
 /opt/spark/sbin/start-history-server.sh
 /opt/spark/sbin/start-worker.sh spark://spark-master:7077
+mlflow server --host 0.0.0.0 --port 3000 & \
 jupyter lab --allow-root --config $SPARK_HOME/jupyter_lab_config.py
-
