0a35551615c0b23aee428a1a1cd666a42dedee54
Melting-Face <ymist17@gmail.com>
2025-09-20 21:32:24 +0900

    add spark connect

--- standalone_hive/spark/dockerfile.d/src/entrypoint.sh
+++ standalone_hive/spark/dockerfile.d/src/entrypoint.sh
@@ -3,6 +3,8 @@
 /opt/spark/sbin/start-master.sh
 /opt/spark/sbin/start-thriftserver.sh \
   --master spark://spark-master:7077
+/opt/spark/sbin/start-connect-server.sh \
+  --master spark://spark-master:7077
 /opt/spark/sbin/start-history-server.sh
 /opt/spark/sbin/start-worker.sh spark://spark-master:7077
 mlflow server --host 0.0.0.0 --port 3000 & \
