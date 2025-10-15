2c3c6799d35e3b1ca781bd058067b6471e363475
melting-face <ymist17@gmail.com>
2025-10-15 17:03:34 +0900

    mod: unity catalog 테스트 추가

--- standalone_unitycatalog/spark/dockerfile.d/src/setup.sh
+++ standalone_unitycatalog/spark/dockerfile.d/src/setup.sh
@@ -8,10 +8,10 @@ apt-get install wget -y && \
 
 while read -r line
 do
-  wget "https://repo1.maven.org/maven2/$line" -P /opt/spark/jars
+  wget --quiet "https://repo1.maven.org/maven2/$line" -P /opt/spark/jars
 done < $SPARK_HOME/dependencies.txt
 
-wget "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/jars/spark-nlp-aarch64-assembly-6.1.2.jar" -P /opt/spark/jars
+wget --quiet "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/jars/spark-nlp-aarch64-assembly-6.1.2.jar" -P /opt/spark/jars
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
