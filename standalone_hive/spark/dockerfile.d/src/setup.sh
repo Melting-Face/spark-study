8dbce4beb16f045732f32a26f805e274e7eba45e
melting-face <ymist17@gmail.com>
2025-11-19 09:45:47 +0900

    fix: update setup script to include curl and remove unnecessary installation step

--- standalone_hive/spark/dockerfile.d/src/setup.sh
+++ standalone_hive/spark/dockerfile.d/src/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update -y && \
-apt-get install wget -y && \
+apt-get install wget curl -y && \
   apt-get clean && \
   apt-get autoremove --purge && \
   rm -rf /var/lib/apt/lists/*
@@ -12,7 +12,5 @@ do
 done < $SPARK_HOME/dependencies.txt
 
 wget "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/jars/spark-nlp-aarch64-assembly-6.1.2.jar" -P /opt/spark/jars
-
-curl -LsSf https://astral.sh/uv/install.sh | sh
 
 /root/.local/bin/uv sync --python 3.12
